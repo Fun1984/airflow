b94f6b684e3be0ba5886af461b144fde7edfd510
Fun1984 <sorkejso@gmail.com>
2025-12-17 21:38:58 +0900

    email operator 업로드

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.email import EmailOperator
+
+with DAG( 
+    dag_id = "dags_email_operator",
+    schedule = "* 8 1 * * *", #매월 1일날 아침 8시 작업 
+    start_date = pendulum.datetime(2025, 12, 17, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        to='sorkejso@naver.com',
+        subject='Airflow 성공메일',
+        html_content='Airflow 작업이 완료되었습니다'
+    )
