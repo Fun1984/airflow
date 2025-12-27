5e6d8668658226cb3e9b234985189e521cc56249
Fun1984 <sorkejso@gmail.com>
2025-12-27 10:49:22 +0900

    Config 파라미터 변화 적용

---
+++ dags/dags_cron_trigger_timetable.py
@@ -0,0 +1,22 @@
+from airflow.timedelta.trigger import CronTriggerTimetable
+from airflow.sdk import DAG, task
+from datetime import timedelta
+from pprint import pprint
+
+with DAG(
+		dag_id='dags_cron_trigger_timetable',
+		schedule=CronTriggerTimetable(
+					cron='*/5 * * * *',
+					timezone='Asia/Seoul',
+					interval=timedelta(minutes=5)
+		),
+		tags=['cron']
+) as dag :
+
+	@task(task_id='task_show_context1')
+	def task_show_context1(**context):
+		print('::group::show context variables')
+		pprint(context)
+		print('::endgroup::')
+	
+	task_show_context1()
