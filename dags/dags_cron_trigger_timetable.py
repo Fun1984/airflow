2bbabf271962ba8368f21d7a7f7fa3b27a0e4e38
Fun1984 <sorkejso@gmail.com>
2025-12-27 11:28:38 +0900

    Python오퍼레이터에서 Xcom 사용

--- dags/dags_cron_trigger_timetable.py
+++ dags/dags_cron_trigger_timetable.py
@@ -5,10 +5,10 @@ from pprint import pprint
 
 with DAG(
 		dag_id='dags_cron_trigger_timetable',
-		schedule=CronTriggerTimetable(
+		schedule=CronTriggerTimetable( #해당 라이브러리가 config 설정을 true로 잡아도, datainterval을 고려하지 않음
 					cron='*/5 * * * *',
 					timezone='Asia/Seoul',
-					interval=timedelta(minutes=5)
+					interval=timedelta(minutes=5) ##주석처리하거나, 그냥 써보거나.. #인터벌 강제 주입
 		),
 		tags=['cron']
 ) as dag :
