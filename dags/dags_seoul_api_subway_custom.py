f965e12019a2aa245624c2c6585e23a1ce6aa584
Fun1984 <sorkejso@gmail.com>
2025-12-30 16:42:30 +0900

    Custom operator 개발 실습_실행2

--- dags/dags_seoul_api_subway_custom.py
+++ dags/dags_seoul_api_subway_custom.py
@@ -12,10 +12,13 @@ with DAG(
     catchup=False,
 ) as dag:
     
-    tb_subway_status = SeoulApiToCsvOperator(
+    tb_subway_status = SeoulApiToCsvOperator( 
+        #태스크 수행 주체는 워커컨테이너임. wsl컴퓨터와 연결되어 있지 않음. docker-compose.yaml에 연결해줘야함
+        #vim으로 들어간 후, x-airflow-common: volumes:에서 - ${AIRFLOW_PROJ_DIR:-.}/airflow/files:/opt/airflow/files를 입력해주고
+        # 그 후, (연결설정이 된)airflow 폴더에서 files를 mkdir 해준다. 
         task_id='tb_subway_status',
         dataset_nm = 'CardSubwayTime', #이건 API 확인해서 넣어 줘야함.
-        path='/opt/airflow/files/TbSubwayStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        path='/opt/airflow/files/TbSubwayStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}', #이 부분은 wsl이 아니라, 실행되는 워커컨테이너 내부임.
         file_name='TbSubwayStatus.csv'
     )
     ## 강의는 이것말고 추가로 SeoulApiToCsvOperator를 사용해서 다른 걸 구함. 
