82d0f0c8d68aa457ceb040bbfac1ef9cd29c742d
Fun1984 <sorkejso@gmail.com>
2026-01-06 18:47:54 +0900

    dataset을 이용한 dag트리거_실행_2

--- dags/dags_dataset_consumer_2.py
+++ dags/dags_dataset_consumer_2.py
@@ -1,6 +1,6 @@
 from airflow import Dataset
 from airflow import DAG
-from airflow.operators.Bash import BashOperator
+from airflow.operators.bash import BashOperator
 import pendulum
 
 dataset_dags_dataset_producer_1 = Dataset("dags_dataset_prodcer_1")
