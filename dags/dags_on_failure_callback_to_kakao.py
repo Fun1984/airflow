bb654390576a34d8259e3c765da0e2947f3ae9ba
Fun1984 <sorkejso@gmail.com>
2026-01-08 00:17:30 +0900

    kakao_taskfail_msg_실행

---
+++ dags/dags_on_failure_callback_to_kakao.py
@@ -0,0 +1,30 @@
+from datetime import timedelta
+import pendulum
+from config.on_failure_callback_to_kakao import on_failure_callback_to_kakao
+
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id='dags_on_failure_callback_to_kakao',
+    start_date=pendulum.datetime(2026,1,7,tz='Asia/Seoul'),
+    schedule='*/20 * * * *',
+    catchup=False,
+    default_args={
+        'on_failure_callback':on_failure_callback_to_kakao,
+        'execution_timeout': timedelta(seconds=60)
+    }
+) as dag:
+    task_slp_90 = BashOperator(
+        task_id='task_slp_90',
+        bash_command='sleep 90'
+    )
+    
+    task_ext_1 = BashOperator(
+        trigger_rule='all_done',
+        task_id='task_ext_1',
+        bash_command='exit 1'
+    )
+
+    task_slp_90 >> task_ext_1
---
+++ plugins/config/get_kakao_token.py
@@ -0,0 +1,17 @@
+import requests
+
+client_id = '{restapikey}'
+redirect_uri = 'https://example.com/oauth/2312312'
+authorize_code = '{인가코드}'
+
+token_url = 'https://kauth.kakao.com/oauth/token'
+data = {
+    'grant_type': 'authorization_code',
+    'client_id': client_id,
+    'redirect_uri': redirect_uri,
+    'code': authorize_code
+}
+
+response = requests.post(token_url, data=data)
+tokens = response.json()
+print(tokens)
