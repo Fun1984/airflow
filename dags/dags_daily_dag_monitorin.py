b9f9577186aa686d120c6d8a93ca658032709eb7
Fun1984 <sorkejso@gmail.com>
2026-01-11 16:55:09 +0900

    모니터링 내역 Slack전송하기_실행_4

--- dags/dags_daily_dag_monitorin.py
+++ dags/dags_daily_dag_monitorin.py
@@ -21,7 +21,7 @@ with DAG(
             with closing(conn.cursor()) as cursor:
                 with open('/opt/airflow/files/sqls/daily_dag_monitoring.sql', 'r') as sql_file:
                     cursor.execute("SET TIME ZONE 'Asia/Seoul';")
-                    sql= '/n'.join(sql_file.readlines())
+                    sql= '\n'.join(sql_file.readlines())
                     cursor.execute(sql)
                     rslt = cursor.fetchall()
                     rslt = pd.DataFrame(rslt)
